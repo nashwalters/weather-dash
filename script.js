23548910833d9d80f9f410b8194a276e8b57f852
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-01-03 19:46:05 -0500

    Added function to get city.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+//Function to get search query.
+function search() {
+    $("#search-btn").click(function(e){
+         e.preventDefault();
+         userInput = $("input").val().trim();
+         getWeather(userInput);
+     })
+ }
+ 
